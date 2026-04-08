ddb261d76f77ab48cac42d1f633efd610bf7d104
HaihengDev <haihenglim9@gmail.com>
2026-04-08 23:39:28 +0700

    added: connection with database

--- src/server.js
+++ src/server.js
@@ -1,16 +1,18 @@
 import express from 'express';
-import {createServer} from 'http';
+import { createServer } from 'http';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-const server = createServer(app);
-const PORT = process.env.PORT || 8888;
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${"PORT"}...`);
+connectDB().then(() => {
+  const server = createServer(app);
+  const PORT = process.env.PORT || 8888;
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}...`);
+  });
 });
